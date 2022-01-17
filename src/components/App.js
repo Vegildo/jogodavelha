1e99a2f312c3b4755a89f816d94ebe8b90aacfac
vegildo <leobss12@gmail.com>
2022-01-17 17:01:42 -0300

    comeco do ovo projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>APP jogo da Velha</div>
+        );
+    }
+}
+
+export default App;
